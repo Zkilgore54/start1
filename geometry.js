847c89b53b5cd0a5872b5a7189a4854689db83c6
Zkilgore54 <tigersfan5496@gmail.com>
2020-02-21 14:04:08 -0500

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -6,14 +6,14 @@ var msg = '';    // Message
 if (time >= 12)
 		// Store 10 times table in a variable
 	while (i < 10) {
-		msg += 'The Area of a circle with radius of i + ' x 5 = ' + (i * 5) + '<br />';
+		msg += 'The Area of a circle with radius of i ' ^2 * 3.14 = ' + (i * 5) + '<br />';
 		i++;
 	}
 
 else if ( time < 12)
 	// Store 10 times table in a variable
 	while (i < 10) {
-		msg += 'The Area of a circle with radius of i + ' x 5 = ' + (i * 5) + '<br />';
+		msg += 'The Area of a squard sides of i  ' ^2 = ' + (i * 5) + '<br />';
 		i++;
 	}
 
