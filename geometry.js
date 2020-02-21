30a727eedc306b8d6bdae6eaceeb183bad938800
Zkilgore54 <tigersfan5496@gmail.com>
2020-02-21 12:25:03 -0500

    Add files via upload

---
+++ geometry.js
@@ -0,0 +1,19 @@
+var i = 1;       // Set counter to 1
+var time = 
+var msg = '';    // Message
+
+
+if (time >= 12)
+// Store 5 times table in a variable
+	while (i < 10) {
+		msg += 'The Area of a circle with radius of i + ' x 5 = ' + (i * 5) + '<br />';
+		i++;
+	}
+
+else if ( time < 12)
+	while (i < 10) {
+		msg += 'The Area of a circle with radius of i + ' x 5 = ' + (i * 5) + '<br />';
+		i++;
+	}
+
+document.getElementById('whitespace').innerHTML = msg;
