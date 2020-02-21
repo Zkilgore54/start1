89da913e18f04567d82a3c18abac859fec4ac5d0
Zkilgore54 <tigersfan5496@gmail.com>
2020-02-21 13:38:20 -0500

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -1,16 +1,17 @@
 var i = 1;       // Set counter to 1
-var time = 
+var time =       // Time of day
 var msg = '';    // Message
 
 
 if (time >= 12)
-// Store 5 times table in a variable
+		// Store 10 times table in a variable
 	while (i < 10) {
 		msg += 'The Area of a circle with radius of i + ' x 5 = ' + (i * 5) + '<br />';
 		i++;
 	}
 
 else if ( time < 12)
+	// Store 10 times table in a variable
 	while (i < 10) {
 		msg += 'The Area of a circle with radius of i + ' x 5 = ' + (i * 5) + '<br />';
 		i++;
