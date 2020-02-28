92bde871e32f7d2152155bbdbb6b3dda9479e3e9
Theodore Payne <tpayne@velocity.org>
2020-02-28 15:49:10 -0500

    set time to the current DateTime when script is loaded and used 2 differentways of creating a string with variables

--- geometry.js
+++ geometry.js
@@ -1,19 +1,19 @@
 var i = 1;       // Set counter to 1
-var time =       // Time of day
+var time = new Date()     // Time of day
 var msg = '';    // Message
 
 
-if (time >= 12)
+if (time.getHours() >= 12)
 		// Store 10 times table in a variable
 	while (i < 10) {
-		msg += 'The Area of a circle with radius of i ' ^2 * 3.14 = ' + (i * 5) + '<br />';
+		msg += `The Area of a circle with radius a of ${i} = ${i^2 * 3.14}: Current Hour - ${time.getHours()} <br />`;
 		i++;
 	}
 
-else if ( time < 12)
+else if ( time.getHours() < 12)
 	// Store 10 times table in a variable
 	while (i < 10) {
-		msg += 'The Area of a squard sides of i  ' ^2 = ' + (i * 5) + '<br />';
+		msg += 'The Area of a squard sides of ' + i + ' = ' + i^2 + ' <br />';
 		i++;
 	}
 
